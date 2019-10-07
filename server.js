b84870588b13fa872466b42abe3c39cfa575ed12
Sander Hestvik <sander.hestvik@gmail.com>
2019-10-07 16:35:35 +0200

    Change comments

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
-var cors = require('cors')
 const app = express();
 const port = 3000;
-// app.use(cors());
 app.get("/", (req, res) => res.sendFile(path.join(__dirname + '/APIAuthentication/authPage.html')));
 app.listen(port, () => console.log(`Listening on port ${port}`));
